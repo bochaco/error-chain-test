e572d3ef3a4560cffefa38ba80a1bfaa855ac7c2
bochaco <gabrielviganotti@gmail.com>
2020-12-29 20:26:15 -0300

    Listing error source chain

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,26 @@
 mod data;
 mod io;
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use data::read_data;
-use std::env;
+use std::{env, error::Error};
 
 fn main() -> Result<()> {
     println!("Let's read a JSON file first...");
     let mut args = env::args();
     args.next();
-    read_data(args.next().as_deref())?;
-
-    Ok(())
+    match read_data(args.next().as_deref()) {
+        Err(err) => {
+            println!("BACKTRACE: {:?}", err.backtrace());
+            let mut curr_err = err.source();
+            let mut index = 0;
+            while let Some(src) = curr_err {
+                println!("SOURCE {}: {}", index, src);
+                curr_err = src.source();
+                index += 1;
+            }
+            bail!(err)
+        }
+        Ok(()) => Ok(()),
+    }
 }
